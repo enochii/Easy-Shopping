60366f7a19c772920c599291bc2b7249c11fa3c7
hangshuaibi <1440620378@qq.com>
2020-05-24 22:51:44 +0800

    init

---
+++ js/login.js
@@ -0,0 +1,41 @@
+// import { LOGIN_URL } from "./const";
+
+function handleSubmit() {
+    function sendData(form) {
+        var request = new XMLHttpRequest();
+        request.open('POST', LOGIN_URL);
+        request.responseType = JSON_FORMAT;
+    
+        request.onload = function() {
+            console.log('onload');
+        };
+    
+        var formdata  = new FormData(form);
+    
+        // 数据成功发送
+        request.addEventListener("load", function(event) {
+            data = JSON.parse(event.target.responseText);
+            
+            console.log(data);
+            // 把 token 存到 local storage
+            localStorage.setItem("token", data.payload.token);
+            window.location.href = '../index.html'
+        });
+    
+        // 失败
+        request.addEventListener("error", function(event) {
+            alert('something went wrong');
+        });
+
+        console.log(formdata);
+        // 发送的数据是由用户在表单中提供的
+        request.send(formdata);
+    }
+    
+    var form = document.getElementById("loginForm");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        sendData(form)
+        // alert('登录成功')
+    })
+}
---
+++ js/register.js
@@ -0,0 +1,5 @@
+function register() {
+    // required 属性虽然 出发，但是还是得和 拦截表单 submit 结合--
+    location.href='./login.html';
+    console.log('注册');
+}
